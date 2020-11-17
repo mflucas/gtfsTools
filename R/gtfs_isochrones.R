242fd5a38ed55ec19398c9abcaefcd5cdec58e8f
mflucas <37622750+mflucas@users.noreply.github.com>
2020-11-17 17:59:37 +0100

    Bugfix

--- R/gtfs_isochrones.R
+++ R/gtfs_isochrones.R
@@ -3,6 +3,7 @@
 #' @description gtfs_isochrones Creates one SP Object containg a isochrones calculated from a tidytransit::raptor result. The function uses the minimal travel times from the start stop to all stops as a main input parameter. This isochrone is a not meant to depict exact reachibility of transit in detail. Use opentripplanner for this instead. The goal here is to provide a decent overview of transit access at larger scales (regional, national or continental).
 #' @param raptor_result A result from tidytransit::raptor function
 #' @param stops A stops dataframe obtained from a tidytransit gtfs object: gtfs$stops
+#' @param stop The stop_id (as.character) of the origin stop
 #' @param breaks A vector containing the travel time breaks for the isochrone in seconds.
 #' @param hull_alpha_min The alpha values increase with increasing travel time for better graphic display of the isochrones.
 #' @param buffer_value The shapes are buffered in the end for graphic display. Value in meters.
@@ -15,14 +16,15 @@
 #' @import sf
 #' @import sp
 #' @import tidytransit
+#' @import rgeos
+#'
 #'
 #'
 #' @export
 #'
 #' @examples
 #'
-gtfs_isochrones <- function(raptor_result, stops, breaks=c(3600, 2*3600, 3*3600), hull_alpha_min=0.2, buffer_value=4000){
-
+gtfs_isochrones <- function(raptor_result, stops, stop,breaks=c(3600, 2*3600, 3*3600), hull_alpha_min=0.2, buffer_value=4000){
 
   is_increasing <- function(vec) {
     return(all(diff(vec) > 0))
@@ -52,7 +54,7 @@ gtfs_isochrones <- function(raptor_result, stops, breaks=c(3600, 2*3600, 3*3600)
         # Arcs need to be redefined as strings of points. Work out the number of points to allocate in this arc segment.
         ipoints <- 2 + round(increment * (rowi$theta / 2),0)
         # Calculate coordinates from arc() description for ipoints along the arc.
-        angles <- anglesArc(v, theta)
+        angles <- alphahull::anglesArc(v, theta)
         seqang <- seq(angles[1], angles[2], length = ipoints)
         x <- round(cc[1] + r * cos(seqang),rnd)
         y <- round(cc[2] + r * sin(seqang),rnd)
@@ -83,8 +85,8 @@ gtfs_isochrones <- function(raptor_result, stops, breaks=c(3600, 2*3600, 3*3600)
           coordsj<-cbind(prevx,prevy)
           colnames(coordsj)<-NULL
           # Build as Line and then Lines class
-          linej <- Line(coordsj)
-          linesj[[j]] <- Lines(linej, ID = as.character(j))
+          linej <- sp::Line(coordsj)
+          linesj[[j]] <- sp::Lines(linej, ID = as.character(j))
           j<-j+1
           prevx<-NULL
           prevy<-NULL
@@ -134,20 +136,24 @@ if(!(is_increasing(breaks))) stop(" 'breaks' must be in increasing order")
     stops_sf$cat <- ifelse(stops_sf$travel_time<breaks[i], i, stops_sf$cat)
   }
 
+  stops <- merge(stops, stops_sf[, c("stop_id", "cat")], by="stop_id")
 
 #Now calculate the convex hulls for each break
   hulls <- list()
   for(i in 1:(length(breaks))){
-    hulls[[i]] <- alphahull::ahull(stops[stops$cat==i, ]$stop_lon, stops[stops$cat==i, ]$stop_lat, alpha = hull_alpha_min+1.5*i/length(breaks))
+    hulls[[i]] <- alphahull::ahull(stops[stops$cat==i, ]$stop_lon, stops[stops$cat==i, ]$stop_lat, alpha = hull_alpha_min+0.5*i*i/length(breaks))
   }
+
+
+
 
 
   #Now transform ahull objects to sp objects:
   hull_sp <- lapply(hulls, FUN=ah2sp)
 
   #And then to sf objects:
-  hull_sf <- lapply(hull_sp, FUN=st_as_sf)
-  hull_sf <- lapply(hull_sf, FUN=st_set_crs, 4326)
+  hull_sf <- lapply(hull_sp, FUN=sf::st_as_sf)
+  hull_sf <- lapply(hull_sf, FUN=sf::st_set_crs, 4326)
 
 #Now refine the display of each isochrone by unioning the alpha hull with the buffered circles around stops:
 
@@ -156,17 +162,17 @@ if(!(is_increasing(breaks))) stop(" 'breaks' must be in increasing order")
   buffer <- stops_sf
 
   #Transform to UTM for calculating buffer
-  buffer <- st_transform(buffer, 3045)
-  hull_sf_buff <- lapply(hull_sf, FUN=st_transform, crs=3045)
+  buffer <- sf::st_transform(buffer, 3045)
+  hull_sf_buff <- lapply(hull_sf, FUN=sf::st_transform, crs=3045)
 
   #Create list of unioned isochrones
   buffererd_shapes <- list()
   for(j in 1:(length(breaks))){
-    points_buff <- st_buffer(buffer[buffer$cat==j,] , buffer_value)
-    points_union <- st_union(points_buff)
-    hull_buff <- st_buffer(hull_sf_buff[[j]], buffer_value)
+    points_buff <- sf::st_buffer(buffer[buffer$cat==j,] , buffer_value)
+    points_union <- sf::st_union(points_buff)
+    hull_buff <- sf::st_buffer(hull_sf_buff[[j]], buffer_value)
 
-    buffererd_shapes[[j]] <- st_union(points_union, hull_buff)
+    buffererd_shapes[[j]] <- sf::st_union(points_union, hull_buff)
 
   }
 
@@ -174,17 +180,17 @@ if(!(is_increasing(breaks))) stop(" 'breaks' must be in increasing order")
   for(i in (length(breaks)):1){
     for(j in (length(breaks)):1){
       if(!(i==j)){
-        buffererd_shapes[[i]] <- st_difference(buffererd_shapes[[i]], buffererd_shapes[[j]])
+        buffererd_shapes[[i]] <- sf::st_difference(buffererd_shapes[[i]], buffererd_shapes[[j]])
       }
     }
   }
 
   #Convert back to WGS84
-  buffererd_shapes <- lapply(buffererd_shapes, FUN=st_transform, crs=4326)
+  buffererd_shapes <- lapply(buffererd_shapes, FUN=sf::st_transform, crs=4326)
 
 
 #Last step: convert it all to one single SP object, which can also be saved as one shapefile
-  sp <- lapply(gtfs_isochrones, as, "Spatial")
+  sp <- lapply(buffererd_shapes, as, "Spatial")
 
   sp_isochrone <- sp[[1]]
   sp_isochrone$break_value <- breaks[1]/3600
